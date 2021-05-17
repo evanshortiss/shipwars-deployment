62e9f3ef5c4565983359ed6d3ff5bd40eba8ef08
Evan Shortiss <evanshortiss@gmail.com>
2021-05-17 21:05:05 +0100

    feat: add openshift deployment script

---
+++ openshift/deploy.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+NAMESPACE=${NAMESPACE:-shipwars}
+CONFIG_MAP_NAME=${CONFIG_MAP_NAME:-shipwars-shared-config}
+LOG_LEVEL=${LOG_LEVEL:-info}
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+oc new-project $NAMESPACE > /dev/null 2>&1
+
+oc process -f "${DIR}/config-map.yml" \
+-p NAMESPACE="${NAMESPACE}" \
+-p CONFIG_MAP_NAME="${CONFIG_MAP_NAME}" \
+-p LOG_LEVEL="${LOG_LEVEL}" | oc create -f -
+
+oc process -f "${DIR}/shipwars-move-server.yml" \
+-p NAMESPACE="${NAMESPACE}" \
+-p CONFIG_MAP_NAME="${CONFIG_MAP_NAME}" | oc create -f -
+
+oc process -f "${DIR}/shipwars-game-server.yml" \
+-p NAMESPACE="${NAMESPACE}" \
+-p CONFIG_MAP_NAME="${CONFIG_MAP_NAME}" | oc create -f -
+
+oc process -f "${DIR}/shipwars-bot-server.yml" \
+-p NAMESPACE="${NAMESPACE}" \
+-p CONFIG_MAP_NAME="${CONFIG_MAP_NAME}" | oc create -f -
+
+oc process -f "${DIR}/shipwars-client.yml" \
+-p NAMESPACE="${NAMESPACE}" \
+-p CONFIG_MAP_NAME="${CONFIG_MAP_NAME}" | oc create -f -
