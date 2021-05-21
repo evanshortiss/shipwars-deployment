a1640e935842db043fe10c68bc34218ca7166599
Evan Shortiss <evanshortiss@gmail.com>
2021-05-21 19:55:31 +0100

    chore: add logging fix: check if project exists prior to create attempt

--- openshift/deploy.sh
+++ openshift/deploy.sh
@@ -6,13 +6,22 @@ LOG_LEVEL=${LOG_LEVEL:-info}
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-oc new-project $NAMESPACE > /dev/null 2>&1
+oc project $NAMESPACE > /dev/null 2>&1
 
+if [ $? -ne 0 ]
+then
+  echo "Creating ${NAMESPACE} project..."
+  # Project did not exist, so we need to create it
+  oc new-project $NAMESPACE > /dev/null 2>&1
+fi
+
+echo "Creating ${CONFIG_MAP_NAME} ConfigMap..."
 oc process -f "${DIR}/config-map.yml" \
 -p NAMESPACE="${NAMESPACE}" \
 -p CONFIG_MAP_NAME="${CONFIG_MAP_NAME}" \
 -p LOG_LEVEL="${LOG_LEVEL}" | oc create -f -
 
+echo "Creating services, deployments, and routes..."
 oc process -f "${DIR}/shipwars-move-server.yml" \
 -p NAMESPACE="${NAMESPACE}" \
 -p CONFIG_MAP_NAME="${CONFIG_MAP_NAME}" | oc create -f -
